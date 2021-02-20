4dc20ee3f6cecd648d76c521d724608da3270b5f
IK-VV-201904079\user <deyew.vladimir@yandex.ru>
2021-02-20 09:41:18 +0700

    сделано черновое оформление разделения списка на Сегодня, Завтра, Предстоящие.

--- lib/screens/tasks/task_page.dart
+++ lib/screens/tasks/task_page.dart
@@ -1,9 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'package:rflutter_alert/rflutter_alert.dart';
 import 'package:ultimate_task_by_studio/misc/constants.dart';
+import 'package:ultimate_task_by_studio/misc/converts.dart';
 import 'package:ultimate_task_by_studio/misc/show_exception_dialog.dart';
 import 'package:ultimate_task_by_studio/misc/show_message.dart';
 import 'package:ultimate_task_by_studio/models/task.dart';
@@ -48,6 +50,10 @@ class TasksPage extends StatefulWidget {
 class _TasksPageState extends State<TasksPage> {
   bool isSwitched = false;
   bool isAnythingForDone = false;
+
+  List<Task> tasksToday = [];
+  List<Task> tasksTomorrow = [];
+  List tasksFuture = [];
 
   Future<void> _signOut(BuildContext context) async {
     try {
@@ -127,7 +133,7 @@ class _TasksPageState extends State<TasksPage> {
         centerTitle: true,
         actions: <Widget>[
           Switch(
-            // inactiveTrackColor: Color(myMintColor),
+              // inactiveTrackColor: Color(myMintColor),
               inactiveThumbColor: Color(myBackgroundColor),
               inactiveTrackColor: Color(myBlackLightColor),
               activeColor: Color(myBlueLightColor),
@@ -149,107 +155,111 @@ class _TasksPageState extends State<TasksPage> {
       ),
       floatingActionButton: !isSwitched
           ? FloatingActionButton(
-        backgroundColor: Color(myBlackLightColor),
-        child: Icon(Icons.add),
-        onPressed: () => AddTaskPage.show(context),
-      )
+              backgroundColor: Color(myBlackLightColor),
+              child: Icon(Icons.add),
+              onPressed: () => AddTaskPage.show(context),
+            )
           : FloatingActionButton(
-        onPressed: () async {
-          if (isAnythingForDone) {
-            Alert(
-              context: context,
-              type: AlertType.warning,
-              title: "",
-              desc: "Удалить завершенные задачи?",
-              buttons: [
-                DialogButton(
-                  child: Text(
-                    "Отмена",
-                    style: TextStyle(color: Colors.white, fontSize: 20),
-                  ),
-                  onPressed: () => Navigator.pop(context),
-                  width: 120,
-                ),
-                DialogButton(
-                  color: Colors.red,
-                  child: Text(
-                    "Удалить",
-                    style: TextStyle(color: Colors.white, fontSize: 20),
-                  ),
-                  onPressed: () {
-                    final database =
-                    Provider.of<Database>(context, listen: false);
-                    database.deleteAllDone();
-                    showMessage(context, "Завершенные задачи удалены");
-                    Navigator.of(context).pop(true);
-                  },
-                  width: 120,
-                )
-              ],
-            ).show();
-          }
-          // final finalDeleting = await showAlertDialog(context,
-          //     title: "Удаление задач",
-          //     content: "Удалить все задачи?",
-          //     defaultActionText: "Удалить",
-          //     cancelActionText: "Отмена");
+              onPressed: () async {
+                if (isAnythingForDone) {
+                  Alert(
+                    context: context,
+                    type: AlertType.warning,
+                    title: "",
+                    desc: "Удалить завершенные задачи?",
+                    buttons: [
+                      DialogButton(
+                        child: Text(
+                          "Отмена",
+                          style: TextStyle(color: Colors.white, fontSize: 20),
+                        ),
+                        onPressed: () => Navigator.pop(context),
+                        width: 120,
+                      ),
+                      DialogButton(
+                        color: Colors.red,
+                        child: Text(
+                          "Удалить",
+                          style: TextStyle(color: Colors.white, fontSize: 20),
+                        ),
+                        onPressed: () {
+                          final database =
+                              Provider.of<Database>(context, listen: false);
+                          database.deleteAllDone();
+                          showMessage(context, "Завершенные задачи удалены");
+                          Navigator.of(context).pop(true);
+                        },
+                        width: 120,
+                      )
+                    ],
+                  ).show();
+                }
+                // final finalDeleting = await showAlertDialog(context,
+                //     title: "Удаление задач",
+                //     content: "Удалить все задачи?",
+                //     defaultActionText: "Удалить",
+                //     cancelActionText: "Отмена");
 
-          // if (finalDeleting == true) {
-          //
-          // }
-        },
-        backgroundColor: Color(myRedColor),
-        child: Icon(
-          Icons.delete_sharp,
-          color: Colors.black,
-        ),
-      ),
+                // if (finalDeleting == true) {
+                //
+                // }
+              },
+              backgroundColor: Color(myRedColor),
+              child: Icon(
+                Icons.delete_sharp,
+                color: Colors.black,
+              ),
+            ),
       body: _buildContexts(context),
     );
   }
 
-  List<Dismissible> getChildren(List tasks) {
-    final children = tasks
-        .map((task) => Dismissible(
-      background: Container(
-        color: Color(myBackgroundColor),
-        child: Padding(
-          padding: const EdgeInsets.all(15),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: <Widget>[
-              Icon(
-                Icons.delete,
-                color: Colors.red,
-              ),
-              SizedBox(
-                width: 10,
-              ),
-            ],
-          ),
-        ),
-      ),
-      key: Key('task-${task.id}'),
-      direction: DismissDirection.endToStart,
-      onDismissed: (direction) =>
-          _delete(context, task, isSwitched ? true : false),
-      child: Padding(
-        padding: const EdgeInsets.all(3.0),
-        child: TaskListTile(
-          context: context,
-          task: task,
-          onTap: () => EditTaskPage.show(context, task: task),
-        ),
-      ),
-    ))
-        .toList();
-
-
-    return children;
-  }
+  // List<Dismissible> getChildren(List tasks) {
+  //   final children = tasks
+  //       .map((task) => Dismissible(
+  //             background: Container(
+  //               color: Color(myBackgroundColor),
+  //               child: Padding(
+  //                 padding: const EdgeInsets.all(15),
+  //                 child: Row(
+  //                   mainAxisAlignment: MainAxisAlignment.end,
+  //                   children: <Widget>[
+  //                     Icon(
+  //                       Icons.delete,
+  //                       color: Colors.red,
+  //                     ),
+  //                     SizedBox(
+  //                       width: 10,
+  //                     ),
+  //                   ],
+  //                 ),
+  //               ),
+  //             ),
+  //             key: Key('task-${task.id}'),
+  //             direction: DismissDirection.endToStart,
+  //             onDismissed: (direction) =>
+  //                 _delete(context, task, isSwitched ? true : false),
+  //             child: Padding(
+  //               padding: const EdgeInsets.all(3.0),
+  //               child: TaskListTile(
+  //                 context: context,
+  //                 task: task,
+  //                 onTap: () => EditTaskPage.show(context, task: task),
+  //               ),
+  //             ),
+  //           ))
+  //       .toList();
+  //
+  //   return children;
+  // }
 
   Widget _buildContexts(BuildContext context) {
     final database = Provider.of<Database>(context, listen: false);
+    var indexTomorrow = 0;
+    bool flagTommorow = false;
+    bool flagFuture = false;
+    var indexFuture = 0;
+
     return StreamBuilder<List<Task>>(
       stream: database.tasksStream(),
       builder: (context, snapshot) {
@@ -260,39 +270,146 @@ class _TasksPageState extends State<TasksPage> {
 
           tasks.forEach((element) {
             if (element.isDeleted == false) {
-
               undoneTasks.add(element);
             } else {
               doneTasks.add(element);
             }
           });
 
-
-          undoneTasks.forEach((element) {
-            if (element.doingDate.isBefore(DateTime.now())) {
-              element.doingDate = DateTime.now();
-            }
-          });
-
           undoneTasks.sort((a, b) => a.doingDate.compareTo(b.doingDate));
           doneTasks.sort((a, b) => a.doingDate.compareTo(b.doingDate));
+
+          var now = DateTime.now();
+          var tomorrow = now.add(new Duration(days: 1));
+          var nextDayAfterTomorrow = now.add(new Duration(days: 2));
+
+          print('~~ today is ${convertFromDateTimeToString(now)}');
+          print('~~ tomorrow is ${convertFromDateTimeToString(tomorrow)}');
+          print(
+              '~~ newtDayAfterTomorrow is ${convertFromDateTimeToString(nextDayAfterTomorrow)}');
+          Duration diff;
+
+          // undoneTasks.forEach((element)
+          for (int i = 0; i < undoneTasks.length; i++) {
+            Task element = undoneTasks[i];
+            print("\n\tindex = $i");
+
+            /*просроченные задачи выставляем в "сегодня"*/
+            if (element.doingDate.isBefore(now)) {
+              element.doingDate = now;
+            }
+
+            print(
+                "\x1B[33m \t\tday is ${convertFromDateTimeToString(element.doingDate)}\x1B[0m");
+            diff = element.doingDate.difference(nextDayAfterTomorrow);
+            print("~ NextDayAfterTomorrow diff is ${diff.inDays}");
+
+            if (diff.inDays == -1) {
+              // tasksTomorrow.add(element);
+              if (flagTommorow == false) {
+                indexTomorrow = i;
+                flagTommorow = true;
+                print(
+                    "\t\t\t indexTomorrow = $indexTomorrow / flagTomorrow = $flagTommorow");
+              }
+
+              print(
+                  "\x1B[31m /tomorrow/ = ${convertFromDateTimeToString(element.doingDate)}\x1B[0m");
+              continue;
+            }
+
+            // diff = element.doingDate.difference(now);
+            // print("~ now diff is ${diff.inDays}");
+
+            if (element.doingDate.isBefore(tomorrow)) {
+              print(
+                  "\x1B[36m /today/ = ${convertFromDateTimeToString(element.doingDate)}\x1B[0m");
+              //tasksToday.add(element);
+            }
+
+            if (element.doingDate.isAfter(tomorrow)) {
+              print(
+                  "\x1B[34m /future/ = ${convertFromDateTimeToString(element.doingDate)}\x1B[0m");
+              // tasksFuture.add(element);
+              if (flagFuture == false) {
+                flagFuture = true;
+                indexFuture = i;
+                print(
+                    "\t\t\t indexFuture = $indexFuture / flagFuture = $flagFuture");
+              }
+
+              print("\n");
+            }
+          }
+          ;
 
           switch (isSwitched) {
             case true:
               if (doneTasks.isNotEmpty) {
-                final children = getChildren(doneTasks);
+                // final children = getChildrenToday(doneTasks);
                 isAnythingForDone = true;
-                return ListView(children: children);
+                // return ListView(children: children);
               } else {
                 isAnythingForDone = false;
               }
               break;
             case false:
               if (undoneTasks.isNotEmpty) {
-                final children = getChildren(undoneTasks);
+                // final childrenToday = getChildren(tasksToday);
+                // final childrenTomorrow = getChildren(tasksTomorrow);
+                // final childrenFuture = getChildren(tasksFuture);
 
-
-                return ListView(children: children);
+                return ListView.separated(
+                  itemCount: undoneTasks.length,
+                  itemBuilder: (context, i) {
+                    // return Card(child: Text(undoneTasks[i].memo),);
+                    return Dismissible(
+                      background: Container(
+                        color: Color(myBackgroundColor),
+                        child: Padding(
+                          padding: const EdgeInsets.all(15),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: <Widget>[
+                              Icon(
+                                Icons.delete,
+                                color: Colors.red,
+                              ),
+                              SizedBox(
+                                width: 10,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      key: Key('task-${undoneTasks[i].id}'),
+                      direction: DismissDirection.endToStart,
+                      onDismissed: (direction) => _delete(
+                          context, undoneTasks[i], isSwitched ? true : false),
+                      child: Padding(
+                        padding: const EdgeInsets.all(3.0),
+                        child: TaskListTile(
+                          context: context,
+                          task: undoneTasks[i],
+                          onTap: () =>
+                              EditTaskPage.show(context, task: undoneTasks[i]),
+                        ),
+                      ),
+                    );
+                  },
+                  separatorBuilder: (context, i) {
+                    if (indexTomorrow == i + 1) {
+                      flagTommorow = false;
+                      return dateSeparator("Завтра");
+                    }
+                    if (indexFuture == i + 1) {
+                      flagFuture = false;
+                      return dateSeparator("Предстоящие");
+                    }
+                    return Container();
+                    // return dateSeparator("Завтра");
+                  },
+                );
               }
               break;
             default:
@@ -302,15 +419,39 @@ class _TasksPageState extends State<TasksPage> {
           return !isSwitched
               ? EmptyContent()
               : EmptyContent(
-            title: 'Завершенные задачи',
-            message: '',
-          );
+                  title: 'Завершенные задачи',
+                  message: '',
+                );
+
         }
         if (snapshot.hasError) {
           return Center(child: Text('ERROR'));
         }
         return Center(child: CircularProgressIndicator());
       },
+    );
+  }
+
+  Row dateSeparator(String text) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Expanded(
+          child: Divider(
+            thickness: 2,
+            indent: 12,
+            endIndent: 12,
+          ),
+        ),
+        Text(text),
+        Expanded(
+          child: Divider(
+            thickness: 2,
+            indent: 12,
+            endIndent: 12,
+          ),
+        ),
+      ],
     );
   }
 }
